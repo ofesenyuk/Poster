de5cb4891f823c58c7bce20f1690222dcae716e2
ofesenyuk <ofesenyuk@ukr.net>
2023-11-10 12:56:40 +0200

    folder renaming

---
+++ src/test/groovy/com/sf/poster/PosterLikeTwitterApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.sf.poster
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PosterLikeTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
